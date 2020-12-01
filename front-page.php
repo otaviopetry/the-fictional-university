9ac5bd9a49cc5fa9781ce22d219f514ab1232d6b
otaviopetry <otaviopetry@gmail.com>
2020-12-01 18:55:21 -0300

    Order custom query by custom field value
    Front-page displays only future events and order ir ascending

--- front-page.php
+++ front-page.php
@@ -15,10 +15,22 @@
     <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-        <?php 
+        <?php
+            $today = date('Ymd'); 
             $homepageEvents = new WP_Query(array(
-                'posts_per_page' => 2,
-                'post_type' => 'event'
+                'posts_per_page' => -1,
+                'post_type' => 'event',
+                'orderby' => 'meta_value',
+                'meta_key' => 'event_date',
+                'order' => 'ASC',
+                'meta_query' => array(
+                    array(
+                        'key' => 'event_date',
+                        'compare' => '>=',
+                        'value' => $today,
+                        'type' => 'numeric'
+                    )
+                )
             ));
 
             while ($homepageEvents->have_posts()) : $homepageEvents->the_post();
