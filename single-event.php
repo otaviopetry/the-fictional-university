054c68356d34c0e7859934e0dcf6d7f799d315b0
otaviopetry <otaviopetry@gmail.com>
2020-12-01 19:45:54 -0300

    Past Events Page
    Page template and integration into the site

--- single-event.php
+++ single-event.php
@@ -15,7 +15,28 @@
 <div class="container container--narrow page-section">
 
     <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span class="metabox__main"><?php the_title(); ?></p>
+        <p>
+            <a 
+                class="metabox__blog-home-link" 
+                href="<?php 
+                    $today = date('Ymd');
+                    if ( get_field('event_date') < $today) {
+                        echo site_url('past-events');
+                    } else {
+                        echo get_post_type_archive_link('event');
+                    }
+                ?>"
+            >
+                <i class="fa fa-home" aria-hidden="true"></i>
+                <?php
+                    if ( get_field('event_date') < $today) {
+                        echo 'Back to Past Events';
+                    } else {
+                        echo 'Events Home';
+                    }
+                ?>
+            </a> <span class="metabox__main"><?php the_title(); ?>
+        </p>
     </div>
 
     <div class="generic-content">
