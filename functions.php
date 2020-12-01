7f7882d1f31bd83e0418867d5515a3c30a8eacd7
otaviopetry <otaviopetry@gmail.com>
2020-12-01 19:17:03 -0300

    Edit Events archive default query
    Refine default query through functions file

--- functions.php
+++ functions.php
@@ -45,3 +45,22 @@ function university_post_types () {
 }
 
 add_action('init', 'university_post_types');
+
+function university_adjust_queries ($query) {
+    $today = date('Ymd');
+    if ( !is_admin() AND is_post_type_archive('event') AND $query->is_main_query() ) {
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        ));
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
