d912beaa4b13f42c4dd80add7c3ca8a66da4a891
otaviopetry <otaviopetry@gmail.com>
2020-12-02 20:15:01 -0300

    Display upcoming events for each program page
    Check for the post id inside the related programs custom field value

--- single-program.php
+++ single-program.php
@@ -25,6 +25,64 @@
     <div class="generic-content">
         <?php the_content(); ?>
     </div>
+
+    <?php
+        $today = date('Ymd'); 
+        $homepageEvents = new WP_Query(array(
+            'posts_per_page' => 2,
+            'post_type' => 'event',
+            'orderby' => 'meta_value_num',
+            'meta_key' => 'event_date',
+            'order' => 'ASC',
+            'meta_query' => array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '>=',
+                    'value' => $today,
+                    'type' => 'numeric'
+                ),
+                array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . get_the_ID() . '"' 
+                    // here we add the quotes to check for the exact post id which will be wrapped in quotes inside the serialized array
+                )
+            )
+        ));
+
+        while ($homepageEvents->have_posts()) : $homepageEvents->the_post();
+    ?>
+    <div class="event-summary">
+        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+            <span class="event-summary__month">
+                <?php 
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M');
+                ?>
+            </span>
+            <span class="event-summary__day">
+                <?php
+                    echo $eventDate->format('d');
+                ?>
+            </span>
+        </a>
+        <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p>
+                <?php 
+                    if (has_excerpt()) {
+                        // use get_the_excerpt here to deal with markup ourselves
+                        echo get_the_excerpt();
+                    } else {
+                        echo wp_trim_words(get_the_content(), 18);
+                    }
+                ?>                
+                <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
+            </p>
+        </div>
+    </div>
+
+    <?php endwhile; ?>
 </div>
 
 <?php endwhile; ?>
