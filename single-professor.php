76a07ff165492837782e6cd948bb74634339e39b
otaviopetry <otaviopetry@gmail.com>
2020-12-05 18:49:14 -0300

    Page Banner component

--- single-professor.php
+++ single-professor.php
@@ -2,32 +2,18 @@
 
 <?php while (have_posts()) : the_post() ?>
 
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php 
-            $pageBannerImage = get_field('page_banner_background_image');
-            echo $pageBannerImage['sizes']['page-banner'];
-        ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-        <p><?php the_field('page_banner_subtitle'); ?></p>
-        </div>
-    </div>  
-</div>
+<?php page_banner(); ?>
 
 <div class="container container--narrow page-section">
 
     <div>
-        
+
         <div class="generic-content">
             <div class="row group">
                 <div class="one-third"><?php the_post_thumbnail('professor-portrait'); ?></div>
                 <div class="two-thirds"><?php the_content(); ?></div>
             </div>
         </div>
-
-        
-        
         
         <?php
             $relatedPrograms = get_field('related_programs');
