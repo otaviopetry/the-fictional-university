efae0b9fa47834cc7ad629345858c5881ff4e474
otaviopetry <otaviopetry@gmail.com>
2020-12-05 19:33:24 -0300

    Implement Event summary template part

--- single-program.php
+++ single-program.php
@@ -89,39 +89,11 @@
 
         <h2 class="headline headline--medium">Upcoming <?php echo get_the_title(); ?> events</h2>
 
-        <?php while ($programEvents->have_posts()) : $programEvents->the_post(); ?>
-        
-        <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month">
-                    <?php 
-                        $eventDate = new DateTime(get_field('event_date'));
-                        echo $eventDate->format('M');
-                    ?>
-                </span>
-                <span class="event-summary__day">
-                    <?php
-                        echo $eventDate->format('d');
-                    ?>
-                </span>
-            </a>
-            <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p>
-                    <?php 
-                        if (has_excerpt()) {
-                            // use get_the_excerpt here to deal with markup ourselves
-                            echo get_the_excerpt();
-                        } else {
-                            echo wp_trim_words(get_the_content(), 18);
-                        }
-                    ?>                
-                    <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
-                </p>
-            </div>
-        </div>
-
-        <?php endwhile; ?>
+        <?php 
+            while ($programEvents->have_posts()) : $programEvents->the_post();
+                get_template_part('template-parts/content-event');
+            endwhile; 
+        ?>
 
     <?php endif; wp_reset_postdata(); ?>
 </div>
