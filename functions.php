8cddbf814607a9cba80dc596148a7e9a4a958de3
otaviopetry <otaviopetry@gmail.com>
2020-12-06 21:50:39 -0300

    Campus Post Type

--- functions.php
+++ functions.php
@@ -109,6 +109,24 @@ function university_post_types () {
         'menu_icon' => 'dashicons-welcome-learn-more'
     ));
 
+    // CAMPUS POST TYPE
+    register_post_type('campus', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'excerpt'),
+        'rewrite' => array('slug' => 'campuses'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Campuses',
+            'add_new_item' => 'Add New Campus',
+            'edit_item' => 'Edit Campus',
+            'all_items' => 'All Campuses',
+            'singular_name' => 'Campus'
+        ),
+        'menu_icon' => 'dashicons-location-alt'
+    ));
+    
+
 }
 
 add_action('init', 'university_post_types');
