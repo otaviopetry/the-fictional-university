239e24ef968da594e78a10a8f21bf41e9c62dd91
otaviopetry <otaviopetry@gmail.com>
2020-12-08 19:53:35 -0300

    Improvements to Search class
    Avoid body scrolling when overlay active, respond to keyboard events

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -7,6 +7,7 @@ class Search {
 		this.openButton = $(".js-search-trigger");
 		this.closeButton = $(".search-overlay__close");
 		this.searchOverlay = $(".search-overlay");
+		this.isOpen = false;
 
 		// Fire event listeners when the Search object is created
 		this.events();
@@ -16,16 +17,34 @@ class Search {
 	events() {
 		this.openButton.on('click', this.openOverlay.bind(this));
 		this.closeButton.on('click', this.closeOverlay.bind(this));
+
+		$(document).on('keydown', this.keyPressDispatcher.bind(this));
 	}
 
 	
 	// 3. Finally, the methods that execute the actions
 	openOverlay() {
 		this.searchOverlay.addClass("search-overlay--active");
+		$("body").addClass("body-no-scroll");
+		console.log('Our open method just ran.');
+		this.isOpen = true;
 	}
 
 	closeOverlay() {
 		this.searchOverlay.removeClass("search-overlay--active");
+		$("body").removeClass("body-no-scroll");
+		console.log('Our close method just ran.');
+		this.isOpen = false;
+	}
+
+	keyPressDispatcher(event) {
+		if (event.keyCode === 83 && !this.isOpen) {
+			this.openOverlay();
+		}
+
+		if (event.keyCode == 27 && this.isOpen) {
+			this.closeOverlay();
+		}
 	}
 }
 
