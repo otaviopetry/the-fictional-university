a66637e63fb0dacc42cc274e8681d058a86b8692
otaviopetry <otaviopetry@gmail.com>
2020-12-12 22:18:00 -0300

    Custom search cover all post types
    Categories: general info, professors, programs, events, campuses

--- includes/search-route.php
+++ includes/search-route.php
@@ -10,20 +10,57 @@ function universityRegisterSearch () {
 }
 
 function universitySearchResults ($request) {
-	$professors = new WP_Query(array(
-		'post_type' => 'professor',
+	$mainQuery = new WP_Query(array(
+		'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
 		's' => sanitize_text_field($request['term'])
 	));
 
-	$professorResults = array();
+	$response = array(
+		'generalInfo' => array(),
+		'professors' => array(),
+		'programs' => array(),
+		'events' => array(),
+		'campuses' => array()
+	);
 
-	while ($professors->have_posts()) {
-		$professors->the_post();
-		array_push($professorResults, array(
-			'title' => get_the_title(),
-			'permalink' => get_the_permalink()
-		));
+	while ($mainQuery->have_posts()) {
+		$mainQuery->the_post();
+
+		if (get_post_type() == 'post' OR get_post_type() == 'page') {
+			array_push($response['generalInfo'], array(
+				'title' => get_the_title(),
+				'permalink' => get_the_permalink()
+			));	
+		}
+
+		if (get_post_type() == 'professor') {
+			array_push($response['professors'], array(
+				'title' => get_the_title(),
+				'permalink' => get_the_permalink()
+			));	
+		}
+
+		if (get_post_type() == 'program') {
+			array_push($response['programs'], array(
+				'title' => get_the_title(),
+				'permalink' => get_the_permalink()
+			));	
+		}
+
+		if (get_post_type() == 'event') {
+			array_push($response['events'], array(
+				'title' => get_the_title(),
+				'permalink' => get_the_permalink()
+			));	
+		}
+
+		if (get_post_type() == 'campus') {
+			array_push($response['campuses'], array(
+				'title' => get_the_title(),
+				'permalink' => get_the_permalink()
+			));	
+		}
 	}
 
-	return $professorResults;
+	return $response;
 }
