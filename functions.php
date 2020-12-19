f8c4aade3d131c4976604d867692c91d79134d87
otaviopetry <otaviopetry@gmail.com>
2020-12-18 23:55:49 -0300

    Add custom post type Notes
    And template for My Notes page

--- functions.php
+++ functions.php
@@ -147,9 +147,23 @@ function university_post_types () {
             'singular_name' => 'Campus'
         ),
         'menu_icon' => 'dashicons-location-alt'
+	));
+	
+	// NOTE POST TYPE
+    register_post_type('note', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+		'public' => false,
+		'show_ui' => true,
+        'labels' => array(
+            'name' => 'Notes',
+            'add_new_item' => 'Add New Note',
+            'edit_item' => 'Edit Note',
+            'all_items' => 'All Notes',
+            'singular_name' => 'Note'
+        ),
+        'menu_icon' => 'dashicons-welcome-write-blog'
     ));
-    
-
 }
 
 add_action('init', 'university_post_types');
