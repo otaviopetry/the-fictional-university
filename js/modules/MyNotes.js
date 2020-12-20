6da6e335f3b88a56d5a48c2e2eeb6cffbb5678dd
otaviopetry <otaviopetry@gmail.com>
2020-12-19 23:26:35 -0300

    Make MyNotes input fields editable

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -1,31 +1,47 @@
-import $ from 'jquery';
-
+import axios from 'axios';
 class MyNotes {
 	constructor () {
+		axios.defaults.headers.common["X-WP-Nonce"] = universityData.nonce
+		this.deleteButtons = document.querySelectorAll('.delete-note');
+		this.editButtons = document.querySelectorAll('.edit-note');
+
 		this.events();			
 	}
 
 	events () {
-		$('.delete-note').on('click', this.deleteNote);
+		this.deleteButtons.forEach((button) => {
+			button.addEventListener('click', this.deleteNote);
+		})
+		this.editButtons.forEach((button) => {
+			button.addEventListener('click', this.editNote);
+		})
 	}
 
 	// Methods will go below
-	deleteNote () {
-		$.ajax({
-			beforeSend: (xhr) => {
-				xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
-			},
-			url: universityData.root_url + '/wp-json/wp/v2/note/98',
-			type: 'DELETE',
-			success: (response) => {
-				console.log('Note deleted sucessfully.');
+	editNote (event) {
+		const thisNote = event.target.closest('li');
+
+		thisNote.querySelectorAll('.note-title-field, .note-body-field').forEach(field => {
+			field.removeAttribute('readonly');
+			field.classList.add('note-active-field');
+		});
+		thisNote.querySelector('.update-note').classList.add('update-note--visible')
+	}
+
+	async deleteNote (event) {
+		const thisNote = event.target.closest('li');
+
+		await axios.delete(`${universityData.root_url}/wp-json/wp/v2/note/${thisNote.dataset.id}`)
+			.then(response => {
+				console.log('Note deleted successfully.');
 				console.log(response);
-			},
-			error: (error) => {
+				setTimeout(() => {
+					thisNote.classList.add('fade-out');
+				}, 100)
+			}).catch(error => {
 				console.log('Something went wrong.');
 				console.log(error);
-			}
-		})
+			})
 	}
 }
 
