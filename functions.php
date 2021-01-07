2b3d89bef5c8b449ea5c369bd075a300a80e3c14
otaviopetry <otaviopetry@gmail.com>
2021-01-07 19:04:45 -0300

    chore: Sanitize user input data in My notes

--- functions.php
+++ functions.php
@@ -252,6 +252,11 @@ function ourLoginTitle () {
 add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate ($data) {
+	if ($data['post_type'] == 'note') {
+		$data['post_title'] = sanitize_text_field($data['post_title']);
+		$data['post_content'] = sanitize_textarea_field($data['post_content']);
+	}
+ 
 	if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
 		$data['post_status'] = "private";
 	}
--- page-my-notes.php
+++ page-my-notes.php
@@ -34,7 +34,7 @@
 				<input readonly class="note-title-field" type="text" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>">
 				<span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
 				<span class="delete-note"><i class="fa fa-pencil" aria-hidden="true"></i> Delete</span>
-				<textarea readonly class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+				<textarea readonly class="note-body-field"><?php echo esc_textarea(wp_strip_all_tags(get_the_content())); ?></textarea>
 				<span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
 			</li>
 		<?php endwhile; ?>
